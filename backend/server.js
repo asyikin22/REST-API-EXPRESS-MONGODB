d9cd3cf280505eef249eea36a5c4eefc1a400fca
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-04-17 22:33:13 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const color = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/middleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/routes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
